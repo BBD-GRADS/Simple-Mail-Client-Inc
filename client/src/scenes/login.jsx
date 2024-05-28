306729d063650207cafe19f160561c7ed4ecb439
Christo Kruger <christo.kruger@bbd>
2024-05-28 15:49:25 +0200

    styling for login screen

--- client/src/scenes/login.jsx
+++ client/src/scenes/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { styleReset, AppBar, Toolbar, TextInput, Frame, Button, MenuList, Separator, MenuListItem, Window, WindowHeader, WindowContent} from 'react95';
+import { styleReset, AppBar, Toolbar, TextInput, Frame, Button, MenuList, Separator, MenuListItem, Window, WindowHeader, WindowContent, Hourglass} from 'react95';
 
 import original from 'react95/dist/themes/original';
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
@@ -31,19 +31,25 @@ const GlobalStyles = createGlobalStyle`
 export const Login = (props) => {
     
     return (
-            
-      <Window className='window' style={{position: 'absolute', width: '30vw', left: '35vw', height: '40vh', top: '30vh'}}>
-      <WindowHeader className='window-title'>
-        <span>Log In - Simple Mail Client</span>
-      </WindowHeader>
-      <WindowContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '60%'}}>
-        <p>To use this application, you need to</p>
-        <p> authenticate with Google.</p>
-        <br />
-        <Button>Authenticate</Button>
-      </WindowContent>
-
-     
-    </Window>  
+      <div className='window-container' style={{ display: 'flex', 
+      flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh'}}>
+        <p> The wait is finally over! Introducing...</p>
+        <br/>
+        <p>Mail95</p>
+      <Window className='window' style={{ width: '30vw', height: '40vh'}}>
+        <WindowHeader className='window-title'>
+          <span>Log In - Simple Mail Client</span>
+        </WindowHeader>
+        <WindowContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '80%'}}>
+          <p>To use this application, you need to</p>
+          <p>authenticate with Google.</p>
+          <br />
+          <Button>Authenticate</Button>
+          <Hourglass size={32} style={{ margin: 20 }} />
+        </WindowContent>
+      </Window>
+      <p>Proudly sponsored by Internet Explorer™️</p>
+    </div>
+    
     );
 }
