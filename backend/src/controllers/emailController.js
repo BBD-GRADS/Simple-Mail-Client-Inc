cf3ab09673bb33be6951fc6ffe16c59601390910
JC Mouton <jacques.mouton@bbd.co.za>
2024-05-30 07:49:15 +0200

    send email

--- backend/src/controllers/emailController.js
+++ backend/src/controllers/emailController.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const receivedEmailDAO = require("../Models/ReceivedEmailDAO");
+const { aws, transporter } = require("../config/aws");
 const { awsConfig } = require("../config");
-const aws = require("aws-sdk");
 
 async function getEmails(req, res) {
   try {
@@ -40,11 +40,7 @@ async function fetchEmailFromS3(req, res) {
   };
 
   try {
-    const s3 = new aws.S3({
-      accessKeyId: awsConfig.accessKeyId,
-      secretAccessKey: awsConfig.secretAccessKey,
-      region: awsConfig.region,
-    });
+    const s3 = new aws.S3();
 
     const data = await s3.getObject(params).promise();
     const emailContent = data.Body.toString("utf-8");
@@ -63,7 +59,52 @@ async function fetchEmailFromS3(req, res) {
   }
 }
 
+async function sendEmail(req, res) {
+  const { to, subject, text, html, attachments } = req.body;
+
+  if (!to || !subject || !text) {
+    return res
+      .status(400)
+      .json({ error: "To, subject, and text are required" });
+  }
+
+  const mailOptions = {
+    from: "test@email95.net", //TODO get email from user check if in db ,check what happens if fake email
+    to,
+    subject,
+    text,
+    html, // If you have HTML content, otherwise remove this line
+    attachments:
+      attachments &&
+      attachments.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content, //base64
+      })),
+    ses: {
+      // Optional extra arguments for SendRawEmail
+      // Tags: [
+      //   {
+      //     Name: "tag_name",
+      //     Value: "tag_value",
+      //   },
+      // ],
+    },
+  };
+  //TODO save in sent db and s3
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Message sent: %s", info.messageId);
+    return res
+      .status(200)
+      .json({ message: "Email sent", messageId: info.messageId });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
 module.exports = {
   getEmails,
   fetchEmailFromS3,
+  sendEmail,
 };
--- backend/src/routes/emailRoutes.js
+++ backend/src/routes/emailRoutes.js
@@ -3,11 +3,13 @@ const authenticateSession = require("../middleware/authMiddleware");
 const {
   getEmails,
   fetchEmailFromS3,
+  sendEmail,
 } = require("../controllers/emailController");
 
 const router = express.Router();
 
 router.get("/list", getEmails);
 router.get("/fetch-email", fetchEmailFromS3);
+router.post("/send", sendEmail);
 
 module.exports = router;
